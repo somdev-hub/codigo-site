a5773f17fa14bfc960899d43a6bca17f75f060a1
Somdev Behera <somdevbehera3103@gmail.com>
2023-04-08 23:53:40 +0530

    codigo app section added

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,16 @@ nav.addEventListener("click", () => {
   console.log("clicked");
   navbar.classList.toggle("flex");
 });
+
+const expand = document.querySelector(".expand-button");
+const coordContainer = document.querySelector(".coordinators-container");
+expand.addEventListener("click", () => {
+  if (coordContainer.style.height == "auto") {
+    coordContainer.style.height = "50vh";
+    expand.innerHTML = "Expand";
+  } else {
+    coordContainer.style.height = "auto";
+    expand.innerHTML = "Collapse";
+  }
+  // document.querySelector(".coordinators-container").style.height="auto";
+});
