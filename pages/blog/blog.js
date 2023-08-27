e36905accc404a3deb225ed8db3178979fb7abd8
Somdev Behera <somdevbehera3103@gmail.com>
2023-08-28 00:00:13 +0530

    blog section added

---
+++ pages/blog/blog.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+  // direction: screen.width > 640 ? "horizontal" : "vertical",
+  slidesPerView: "auto",
+  // mouseWeheel: screen.width > 640 ? false : true,
+  spaceBetween: 10,
+  // activeIndex: 1,
+  initialSlide: 0,
+  centeredSlides: false,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
